fe1d83de958f94f172fd30bf3103dda6bd8d4a37
503231259 <l503231259@163.com>
2019-06-22 14:49:08 +0800

    添加home和login视图组件&配置路由导航

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>haha</p>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,12 @@ Vue.use(Router)
 
 export default new Router({
   routes: [{
-    name: '',
-    path: ''
+    name: 'home',
+    path: '/',
+    component: () => import('@/views/home')
+  }, {
+    name: 'login',
+    path: '/login',
+    component: () => import('@/views/login')
   }]
 })
